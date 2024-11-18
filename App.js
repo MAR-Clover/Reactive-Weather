388556b451435670611a46893f433aa6cdd713db
Mohamed Rafeek <mohamed.rafeek@mohamed.rafeek-MacBook-Pro>
2024-11-18 15:39:00 -0500

    updated weather card to show weather properties

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-// Import data and WeatherCard here
-
+import WeatherCard from "./components/WeatherCard";
+const cities = require('./data'); // Ensure correct path for cities data
 
 function App() {
-    return (
-        <>
-            <h1 className = "title">REACTIVE WEATHER</h1>
-            <h3 className = "subtitle">Up to the minute weather news</h3>
-            <div className = "app">
-                {/* Render components here */}
-                
-
-
-            </div>
-            
-        </>
-    )
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {/* Map through cities array and pass the city object to WeatherCard */}
+        {cities.map((city) => (
+          <WeatherCard key={city.city} city={city} />
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default App;
+//export default App;
+module.exports = App
