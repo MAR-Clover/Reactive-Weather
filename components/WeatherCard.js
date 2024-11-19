71762f4660c4b1ac9c4b6014a9004c6aae12f34d
Mohamed Rafeek <mohamed.rafeek@mohamed.rafeek-MacBook-Pro>
2024-11-19 13:29:59 -0500

    added functionality to render images based on forecase

--- components/WeatherCard.js
+++ components/WeatherCard.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import sunny from "../assets/Sunny.svg"
+ import rainy from "../assets/Rainy.svg"
+ import cloudy from "../assets/Cloudy.svg"
+ import partlyCloudy from "../assets/PartlyCloudy.svg"
+
 
 
 function WeatherCard({city}) {
+  const [weather, setWeather] = useState("");
+
+  useEffect(() => {
+    // Update weather when city.forecast changes
+    if (city.forecast === "Sunny") {
+      setWeather(sunny);
+    } else if (city.forecast === "Rainy") {
+      setWeather(rainy);
+    } else if (city.forecast === "Cloudy") {
+      setWeather(cloudy);
+    } else if (city.forecast == "partly Cloudy"){
+      setWeather(partlyCloudy);
+    }
+  }, [city.forecast]); // Dependency array ensures the effect runs when forecast changes
+
   return (
     <div className = "card">
         <div className = "img-container">
-            <img className="card-img-top" src = {sunny} alt="Card image cap" id = "icon"/>
+            <img className="card-img-top" src = {weather} alt="Card image cap" id = "icon"/>
         </div>
         <div class="card-body">
             <h3 className="card-title">{city.city}</h3>
